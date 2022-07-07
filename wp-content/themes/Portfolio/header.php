cb57c6b73a5990adb2bbfa7a2233b708a519ebc5
alishkeir <alishkeir_@hotmail.com>
2022-07-07 13:18:05 +0300

    Added Google Analytics

--- wp-content/themes/Portfolio/header.php
+++ wp-content/themes/Portfolio/header.php
@@ -11,6 +11,20 @@
 <html lang="en">
 
 <head>
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-0XSG6B7149"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-0XSG6B7149');
+  </script>
+
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
@@ -27,6 +41,7 @@
   <!-- Main CSS File -->
   <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() . '/dist/bundle.css' ?>" />
   <?php wp_head() ?>
+  <script async="" src="https://www.googletagmanager.com/gtag/js?id=G-D20WCYZ5ZJ"></script>
 </head>
 
 <body class="dark">
